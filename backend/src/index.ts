2448b025c9c14a9d47442f783c5857f0b4b512ed
MuneerAhmed03 <muneerahmed00916@gmail.com>
2024-05-13 23:06:53 +0530

    dotenv included

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
-import {createServer} from "http";
-import {Server} from "socket.io";
+import { createServer } from "http";
+import { Server } from "socket.io";
 import { initializeSocketIO, getIO } from "./io/io";
 import cors from "cors";
 
@@ -8,21 +8,20 @@ const app = express();
 const port = process.env.PORT || 3000;
 const corsOptions = {
   origin: "http://localhost:5173",
-  optionsSuccessStatus: 200 
-}
+  optionsSuccessStatus: 200,
+};
 app.use(cors(corsOptions));
 
 const server = createServer(app);
 
 initializeSocketIO(server);
 
-const router = require("./routes/index")
+const router = require("./routes/index");
 
 app.use(express.json());
 app.use(express.raw());
 app.use(express.text());
-app.use("/dump",router);
-
+app.use("/dump", router);
 
 server.listen(port, () => {
   console.log("Server is running on ${port} ");
