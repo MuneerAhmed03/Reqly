0c8fb6a0489d240971d1dfb2055c81f2ad66be18
MuneerAhmed03 <muneerahmed00916@gmail.com>
2024-05-14 13:53:03 +0530

    added cors origin

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,7 +7,7 @@ import cors from "cors";
 const app = express();
 const port = process.env.PORT || 3000;
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ['http://localhost:5173','https://reqly.vercel.app'],
   optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
