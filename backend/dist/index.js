0a0a5876fe2d1869c18d687e13ca6487da4f740d
MuneerAhmed03 <muneerahmed00916@gmail.com>
2024-05-14 14:09:54 +0530

    fixed cors origin

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -10,7 +10,7 @@ const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: 'https://reqly.vercel.app',
     optionsSuccessStatus: 200,
 };
 app.use((0, cors_1.default)(corsOptions));
